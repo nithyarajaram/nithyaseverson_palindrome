d6a357b47ea74465826ad695b399a87638df295b
Nithya Rajaram <nithyarajaram@nithyas-air.attlocal.net>
2021-01-04 08:00:25 -0800

    Refactored palindrome

--- lib/nithya_palindrome.rb
+++ lib/nithya_palindrome.rb
@@ -2,7 +2,17 @@
 
 require_relative "nithya_palindrome/version"
 
-module NithyaPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      self.scan(/[a-zA-Z]/).join.downcase
+    end
 end
